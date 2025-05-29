71d130793af8ed8829ec40d98a3d2119e8a11b8c
alex <alexnemi@gmail.com>
2025-05-29 12:26:57 -0300

    login

--- app/page.js
+++ app/page.js
@@ -10,20 +10,20 @@ export default function Home() {
       <main className="flex-1 flex items-center justify-center p-4">
       <Card className="w-full md:w-1/3">
         <CardHeader>
-          <CardTitle>            <Image src="/puente.svg" alt="Puente" width={120} height={120} className="mx-auto" />
+          <CardTitle>            <Image src="/puente.svg" alt="Puente" width={200} height={40} className="mx-auto pb-2" />
           </CardTitle>
           <CardDescription className="text-center">
-          Tu plataforma de trading para invertir y hacer crecer tu capital
+          Tu plataforma de trading para invertir y hacer crecer tu capital.
           </CardDescription>
         </CardHeader>
         <CardContent>
           <form >
-            <div className="mb-4">
+            <div className="mb-4 md:w-1/2 w-full mx-auto">
             <Input type="email" placeholder="Email" />
 
             </div>
 
-            <div className="mb-4">
+            <div className="mb-4 md:w-1/2 w-full mx-auto">
             <Input type="password" placeholder="Contraseña" />
 
             </div>
