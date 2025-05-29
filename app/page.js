9bd041d5a664dc1c67094f86c8434acbd8d72f5c
alex <alexnemi@gmail.com>
2025-05-29 12:23:38 -0300

    boton login

--- app/page.js
+++ app/page.js
@@ -1,21 +1,45 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from "@/components/ui/input";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-8 bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
-      <main className="text-center space-y-8 max-w-2xl">
-        <div className="space-y-4">
-          <h1 className="text-5xl font-bold text-gray-900 dark:text-white">
-            ¡Bienvenido a Puente!
-          </h1>
-          <p className="text-xl text-gray-600 dark:text-gray-300">
-            Tu plataforma para conectar y crear experiencias increíbles
-          </p>
-          <Button>
-            hola
+    <div className="min-h-screen flex flex-col  bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
+      <main className="flex-1 flex items-center justify-center p-4">
+      <Card className="w-full md:w-1/3">
+        <CardHeader>
+          <CardTitle>            <Image src="/puente.svg" alt="Puente" width={120} height={120} className="mx-auto" />
+          </CardTitle>
+          <CardDescription className="text-center">
+          Tu plataforma de trading para invertir y hacer crecer tu capital
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form >
+            <div className="mb-4">
+            <Input type="email" placeholder="Email" />
+
+            </div>
+
+            <div className="mb-4">
+            <Input type="password" placeholder="Contraseña" />
+
+            </div>
+
+            <div className="flex justify-center">
+              <Button >Entrar</Button>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter>
+        <Button variant={"outline"}>
+            Registrarse Ahora
           </Button>
-        </div>
+        </CardFooter>
+      </Card>
+      
         
       
        
